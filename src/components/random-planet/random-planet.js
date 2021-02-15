e91b58ae7d268d89a3d3c05240cb0f4c86ea7885
Dmytro Zhyltsov <zkarman@mail.ru>
2021-02-15 21:06:04 +0200

    feat: add gettting random planet's data from api

--- src/components/random-planet/random-planet.js
+++ src/components/random-planet/random-planet.js
@@ -1,29 +1,58 @@
 import React, { Component } from 'react';
+import SwapiService from '../../services/swapi-service';
 
 import './random-planet.css';
 
 class RandomPlanet extends Component {
+  constructor() {
+    super();
+    this.swapi = new SwapiService();
+    this.updatePlanet();
+    this.state = {
+      planet: {},
+    };
+  }
+
+  onPlanetLoaded = (planet) => {
+    this.setState({ planet });
+  };
+
+  updatePlanet() {
+    const id = Math.floor(Math.random() * 25 + 2);
+    this.swapi
+      .getPlanet(id)
+      .then(this.onPlanetLoaded)
+      .catch((err) => {
+        console.log('Could not fetch, ', err);
+      });
+  }
+
   render() {
+    const {
+      planet: { id, planetName, population, rotationPeriod, diameter },
+    } = this.state;
+
     return (
       <div className="random-planet">
         <img
           className="planet-image"
-          src={`https://starwars-visualguide.com/assets/img/planets/2.jpg`}
+          src={`https://starwars-visualguide.com/assets/img/planets/${id}.jpg`}
+          alt={planetName}
         />
         <div>
-          <h4>Test</h4>
+          <h4>{planetName}</h4>
           <ul className="list-group list-group-flush">
             <li className="list-group-item">
               <span className="term">Population</span>
-              <span>Test</span>
+              <span>{population}</span>
             </li>
             <li className="list-group-item">
               <span className="term">Rotation Period</span>
-              <span>Test</span>
+              <span>{rotationPeriod}</span>
             </li>
             <li className="list-group-item">
               <span className="term">Diameter</span>
-              <span>Test</span>
+              <span>{diameter}</span>
             </li>
           </ul>
         </div>
--- src/services/swapi-service.js
+++ src/services/swapi-service.js
@@ -18,18 +18,18 @@ class SwapiService {
   }
 
   async getPerson(id) {
-    const res = await this.getResource(`/people/${id}/`);
-    return res;
+    const person = await this.getResource(`/people/${id}/`);
+    return this._transformPerson(person);
   }
 
   async getAllPlanets() {
     const res = await this.getResource(`/planets/`);
-    return res.results;
+    return res.results.map(this._transformPlanet);
   }
 
   async getPlanet(id) {
-    const res = await this.getResource(`/planets/${id}/`);
-    return res;
+    const planet = await this.getResource(`/planets/${id}/`);
+    return this._transformPlanet(planet);
   }
 
   async getAllStarsips() {
@@ -38,8 +38,47 @@ class SwapiService {
   }
 
   async getStarship(id) {
-    const res = await this.getResource(`/starships/${id}/`);
-    return res;
+    const starship = await this.getResource(`/starships/${id}/`);
+    return this._transformStarhip(starship);
+  }
+
+  _extractId(item) {
+    const idRegExp = /\/([0-9]*)\/$/;
+    return item.url.match(idRegExp)[1];
+  }
+
+  _transformPlanet(planet) {
+    return {
+      id: this._extractId(planet),
+      planetName: planet.name,
+      population: planet.population,
+      rotationPeriod: planet.rotation_period,
+      diameter: planet.diameter,
+    };
+  }
+
+  _transformStarship(starhip) {
+    return {
+      id: this._extractId(starhip),
+      starshipName: starhip.name,
+      model: starhip.model,
+      manufacturer: starhip.manufacturer,
+      costInCredits: starhip.costInCredits,
+      length: starhip.length,
+      crew: starhip.crew,
+      passengers: starhip.passengers,
+      cargoCapicity: starhip.cargoCapicity,
+    };
+  }
+
+  _transformPerson(person) {
+    return {
+      id: this._extractId(person),
+      personName: person.name,
+      gender: person.gender,
+      birthYear: person.birthYear,
+      eyeColor: person.eyeColor,
+    };
   }
 }
 
