105adc7ffe21d1600c2f1284da6522d8d6ff6092
Dmytro Zhyltsov <zkarman@mail.ru>
2021-02-15 21:28:19 +0200

    feat: add loading spinner

---
+++ src/components/spinner/spinner.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+import './spinner.css';
+
+class Spinner extends Component {
+  render() {
+    return (
+      <div class="loadingio-spinner-double-ring-n0xgs808eei">
+        <div class="ldio-pzzefwwegcn">
+          <div></div>
+          <div></div>
+          <div>
+            <div></div>
+          </div>
+          <div>
+            <div></div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Spinner;
