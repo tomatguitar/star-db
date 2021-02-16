1a6d2c5b24a506ea9840bcdf437e80c14d2838fa
Dmytro Zhyltsov <zkarman@mail.ru>
2021-02-16 13:30:30 +0200

    feat: add list items rendering

--- src/components/item-list/item-list.js
+++ src/components/item-list/item-list.js
@@ -1,15 +1,49 @@
 import React, { Component } from 'react';
+import SwapiService from '../../services/swapi-service';
+import Spinner from '../spinner/spinner';
 
 import './item-list.css';
 
 export default class ItemList extends Component {
+  constructor(props) {
+    super(props);
+    this.swapi = new SwapiService();
+    this.state = {
+      peopleList: null,
+    };
+  }
+
+  componentDidMount() {
+    this.swapi.getAllPeople().then((people) => {
+      this.setState({
+        peopleList: people,
+      });
+    });
+  }
+
+  renderItems = (arr) => {
+    return arr.map(({ id, personName }) => {
+      return (
+        <li
+          className="list-group-item"
+          key={id}
+          onClick={() => {
+            this.props.onItemSelected(id);
+          }}
+        >
+          {personName}
+        </li>
+      );
+    });
+  };
+
   render() {
-    return (
-      <ul className="item-list list-group">
-        <li className="list-group-item">Luke Skywalker</li>
-        <li className="list-group-item">Darth Vader</li>
-        <li className="list-group-item">R2-D2</li>
-      </ul>
-    );
+    const { peopleList } = this.state;
+
+    if (!peopleList) {
+      return <Spinner />;
+    }
+    const items = this.renderItems(peopleList);
+    return <ul className="item-list list-group">{items}</ul>;
   }
 }
