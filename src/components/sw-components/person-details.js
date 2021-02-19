3101196f134c52b315b930dab8723c574e43a6e1
Dmytro Zhyltsov <zkarman@mail.ru>
2021-02-19 19:02:10 +0200

    refactor: move details-components to separate files

---
+++ src/components/sw-components/person-details.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import ItemDetails, { Record } from '../item-details';
+import { SwapiServiceConsumer } from '../swapi-service-context';
+
+const PersonDetails = ({ itemId }) => {
+  return (
+    <SwapiServiceConsumer>
+      {({ getPerson, getPersonImage }) => {
+        return (
+          <ItemDetails
+            itemId={itemId}
+            getData={getPerson}
+            getImageUrl={getPersonImage}
+          >
+            <Record field="gender" label="Gender" />
+            <Record field="eyeColor" label="Eye Color" />
+          </ItemDetails>
+        );
+      }}
+    </SwapiServiceConsumer>
+  );
+};
+
+export default PersonDetails;
